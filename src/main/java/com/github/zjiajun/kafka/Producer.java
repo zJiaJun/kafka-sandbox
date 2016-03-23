efd81e26e375280175255a4bf6556d32e79eed43
zhujiajun <zhujiajun.me@gmail.com>
2016-03-23 21:38:25 +0800

    Initial commit

---
+++ src/main/java/com/github/zjiajun/kafka/Producer.java
@@ -0,0 +1,9 @@
+package com.github.zjiajun.kafka;
+
+/**
+ * Created by zhujiajun
+ * 16/3/23 21:33
+ */
+public class Producer {
+
+}
---
+++ src/main/java/com/github/zjiajun/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by zhujiajun
+ * 16/3/23 21:32
+ */
+package com.github.zjiajun;
